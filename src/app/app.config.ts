861e22cb486ed8d6e3d07e4e2205c1a8596012bb
guilherme.guia <guilhermeguia007@gmail.com>
2025-09-29 23:24:16 -0400

    Feat: app configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { icons } from './icons-provider';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideNzIcons(icons), provideNzI18n(pt_BR), provideAnimationsAsync(), provideHttpClient()
   ]
 };
